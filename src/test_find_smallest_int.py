b41fecb1edca58950953522dd0110a43926ef404
Ben Petty <benjamin.s.petty@gmail.com>
2016-12-18 17:51:43 -0800

    sum of nth and tests

--- src/test_find_smallest_int.py
+++ src/test_find_smallest_int.py
@@ -1,8 +1,8 @@
 """Test find_smallest_int function."""
 
 
-import sys
+# import sys
 
-test.assert_equals(findSmallestInt([78,56,232,12,11,43]), 11)
-test.assert_equals(findSmallestInt([78,56,-2,12,8,-33]), -33)
-test.assert_equals(findSmallestInt([0, -1-sys.maxint,sys.maxint]), -1-sys.maxint)
+# test.assert_equals(findSmallestInt([78,56,232,12,11,43]), 11)
+# test.assert_equals(findSmallestInt([78,56,-2,12,8,-33]), -33)
+# test.assert_equals(findSmallestInt([0, -1-sys.maxint,sys.maxint]), -1-sys.maxint)

71bba0de099cd83f5295894e1bbd3bef68b4afd7
Ben Petty <benjamin.s.petty@gmail.com>
2017-02-10 12:01:38 -0800

    test sum terms

--- src/test_sum_terms.py
+++ src/test_sum_terms.py
@@ -1,17 +1,17 @@
 """Test sum_of_nth."""
 
 
-# import pytest
+import pytest
 
-# TEST_VARS = [
-#     [1, "1.00"],
-#     [2, "1.25"],
-#     [3, "1.39"],
-# ]
+TEST_VARS = [
+    [1, "1.00"],
+    [2, "1.25"],
+    [3, "1.39"],
+]
 
 
-# @pytest.mark.parametrize("param, answer", TEST_VARS)
-# def test_sum_of_nth(param, answer):
-#     """Code wars tests."""
-#     from sum_of_nth import series_sum
-#     assert series_sum(param) == answer
+@pytest.mark.parametrize("param, answer", TEST_VARS)
+def test_sum_of_nth(param, answer):
+    """Code wars tests."""
+    from sum_of_nth import series_sum
+    assert series_sum(param) == answer

abf12797324cb912aaf8029986c7cb6239355b1d
Ben Petty <benjamin.s.petty@gmail.com>
2017-02-10 21:23:40 -0800

    test validate pin

--- src/test_validate_pin.py
+++ src/test_validate_pin.py
@@ -1,28 +1,31 @@
 """Test validate_pin function."""
 
+import pytest
 
-# Test.describe("validate_pin")
+TEST_VARS = [
+    ["1", False],
+    ["12", False],
+    ["123", False],
+    ["12345", False],
+    ["1234567", False],
+    ["1234", True],
+    ["1.234", False],
+    ["00000000", False],
+    ["a234", False],
+    [".234", False],
+    ["1234", True],
+    ["0000", True],
+    ["1111", True],
+    ["123456", True],
+    ["098765", True],
+    ["000000", True],
+    ["123456", True],
+    ["090909", True],
+]
 
-# Test.it("should return False for pins with length other than 4 or 6")
-# Test.assert_equals(validate_pin("1"),False, "Wrong output for '1'")
-# Test.assert_equals(validate_pin("12"),False, "Wrong output for '12'")
-# Test.assert_equals(validate_pin("123"),False, "Wrong output for '123'")
-# Test.assert_equals(validate_pin("12345"),False, "Wrong output for '12345'")
-# Test.assert_equals(validate_pin("1234567"),False, "Wrong output for '1234567'")
-# Test.assert_equals(validate_pin("-1234"),False, "Wrong output for '-1234'")
-# Test.assert_equals(validate_pin("1.234"),False, "Wrong output for '1.234'")
-# Test.assert_equals(validate_pin("00000000"),False, "Wrong output for '00000000'")
 
-# Test.it("should return False for pins which contain characters other than digits")
-# Test.assert_equals(validate_pin("a234"),False, "Wrong output for 'a234'")
-# Test.assert_equals(validate_pin(".234"),False, "Wrong output for '.234'")
-
-# Test.it("should return True for valid pins")
-# Test.assert_equals(validate_pin("1234"),True, "Wrong output for '1234'")
-# Test.assert_equals(validate_pin("0000"),True, "Wrong output for '0000'")
-# Test.assert_equals(validate_pin("1111"),True, "Wrong output for '1111'")
-# Test.assert_equals(validate_pin("123456"),True, "Wrong output for '123456'")
-# Test.assert_equals(validate_pin("098765"),True, "Wrong output for '098765'")
-# Test.assert_equals(validate_pin("000000"),True, "Wrong output for '000000'")
-# Test.assert_equals(validate_pin("123456"),True, "Wrong output for '123456'")
-# Test.assert_equals(validate_pin("090909"),True, "Wrong output for '090909'")
+@pytest.mark.parametrize("param, answer", TEST_VARS)
+def test_validate_pin(param, answer):
+    """Test validate pin."""
+    from validate_pin import vp
+    assert vp(param) == answer

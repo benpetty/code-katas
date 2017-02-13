d66407d422ac76f738b07b27d5e6e2369ef4d4ff
Ben Petty <benjamin.s.petty@gmail.com>
2017-02-12 16:51:05 -0800

    py2 pass

--- src/count_code.py
+++ src/count_code.py
@@ -1,4 +1,4 @@
-u"""String-2 > count_code.
+"""String-2 > count_code.
 
 http://codingbat.com/prob/p186048
 
@@ -7,9 +7,9 @@ anywhere in the given string,
 except we'll accept any letter for the 'd',
 so "cope" and "cooe" count.
 
-count_code('aaacodebbb') → 1
-count_code('codexxcode') → 2
-count_code('cozexxcope') → 2
+count_code('aaacodebbb') -> 1
+count_code('codexxcode') -> 2
+count_code('cozexxcope') -> 2
 """
 
 

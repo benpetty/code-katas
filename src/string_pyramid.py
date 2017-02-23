3b2e64a7022c6d225827ef4b387328257afe4032
Ben Petty <benjamin.s.petty@gmail.com>
2017-02-22 19:35:35 -0800

    base

---
+++ src/string_pyramid.py
@@ -0,0 +1,69 @@
+"""String pyramid.
+
+This pyramid should be built from characters from a given string.
+
+You have to create the code for these four methods:
+characters = 'abc'
+-------------------------------------------------------------------------------
+def watch_pyramid_from_the_side(characters):
+# shows the pyramid as you would see from the side.
+
+  c
+ bbb
+aaaaa
+
+def watch_pyramid_from_above(characters):
+# shows the pyramid as you would see from above.
+
+aaaaa
+abbba
+abcba
+abbba
+aaaaa
+
+def count_visible_characters_of_the_pyramid(characters):
+# should return the count of all characters
+# that are visible from outside the pyramid.
+
+25
+
+def count_all_characters_of_the_pyramid(characters):
+# should count all characters of the pyramid.
+# the pyramid is completely solid and has no holes or rooms in it.
+
+35
+-------------------------------------------------------------------------------
+
+Every character will be used for building one layer of the pyramid.
+So the length of the given string will be the height of the pyramid.
+Every layer will be built with stones from the given character.
+There is no limit of stones.
+The pyramid should have perfect angles of 45 degrees.
+There is no meaning in the order or the choice of the characters.
+It should work the same for example "aaaaa" or "54321".
+Hint: Your output for the side must always be a rectangle!
+So spaces at the end of a line must not be deleted or trimmed!
+If the string is null or empty,
+    you should exactly return this value for the watch-methods
+    and -1 for the count-methods.
+"""
+
+
+def watch_pyramid_from_the_side(characters):
+    """."""
+    pass
+
+
+def watch_pyramid_from_above(characters):
+    """."""
+    pass
+
+
+def count_visible_characters_of_the_pyramid(characters):
+    """."""
+    pass
+
+
+def count_all_characters_of_the_pyramid(characters):
+    """."""
+    pass

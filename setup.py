d1c9065f52a8d3622e4310035e4e74f01cbf0ff7
Ben Petty <22062601+benpetty@users.noreply.github.com>
2020-01-22 14:21:54 -0800

    chng em

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description="My solutions to coding challenges.",
     version=0.1,
     author="Ben Petty",
-    author_email="benjamin.s.petty@gmail.com",
+    author_email="22062601+benpetty@users.noreply.github.com",
     license="MIT",
     extras_require={
         'testing': [
